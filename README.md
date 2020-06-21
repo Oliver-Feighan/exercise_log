f6ea12abef93cc9cfcc4636f962f1e971fd1ebd9
of15641 <of15641@bristol.ac.uk>
2020-06-21 17:24:44 +0100

    try to run INSERT throught python. Add notes to README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# exercise_log
+# exercise log
 GUI for keeping track of exercise - links to an sqlite database file
+
+##general notes
+
+due to free access of jetbrains, I'm developing this in pycharm. Turns out pycharm has great tools for reading
+.db files. To use these, I had to download SQLite JDBC driver, after having a look at data source properties. This
+wasn't necessary to read the file... Pycharm now highlights errors INSERT commands, about what exists in the table.
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+calisthenics(
+LOGDATE DATE NOT NULL,
+PUSHUPS INT NOT NULL,
+PULLUPS INT NOT NULL,
+SQUATS  INT NOT NULL,
+COMMENTS VARCHAR(50) NULL,
+RATING DECIMAL(1,1) NOT NULL,
+PRIMARY KEY (LOGDATE));
+"""
+
 import sqlite3
 
 conn = sqlite3.connect('log.db')
@@ -5,7 +16,13 @@ conn = sqlite3.connect('log.db')
 cur = conn.cursor()
 cur.execute("SELECT * FROM calisthenics")
 
+sqlite_insert_with_param = """INSERT INTO 'calisthenics' 
+('LOGDATE', 'PUSHUPS', 'PULLUPS') values (?, ?, ?, ?, ? ,?)"""
+
+cur.execute("""INSERT INTO calisthenics values (?, ?, ?, ?, ? ,?)""", )
+
 rows = cur.fetchall()
 
 for row in rows:
 	print(row)
+
