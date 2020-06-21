e0189fa49a90a73d47e903d1771b10160d4ce9f0
of15641 <of15641@bristol.ac.uk>
2020-06-21 21:27:05 +0100

    make a data object for calisthenics, write an INSERT function

--- sql_interface/calisthenics_functions.py
+++ sql_interface/calisthenics_functions.py
@@ -1,3 +1,21 @@
 import sqlite3
+import utils.data_objects as data_objects
 
-def write_to_calisthenics(cursor, )
+def write_to_calisthenics(connection : sqlite3.Connection,
+                          data : data_objects.CalisthenicsData
+                          ) -> None:
+    """
+
+
+
+    :param connection: the database connection object
+    :return: no return, just inserts into database
+    """
+    cursor = connection.cursor()
+
+    sqlite_insert_str = \
+        """INSERT INTO calisthenics values (?, ?, ?, ?, ? ,?)"""
+
+    cursor.execute(sqlite_insert_str, data.make_data_dict())
+
+    connection.commit()
---
+++ utils/data_objects.py
@@ -0,0 +1,49 @@
+class CalisthenicsData(object):
+    """
+    the data object that contains the data to be input into the
+    calisthenics table.
+    >>> import datetime
+    >>> calisthenic_data = \
+     CalisthenicsData({ \
+      "logdate" : datetime.datetime.now().date() \
+    , "pushups" : 75 \
+    , "pullups" : 18 \
+    , "squats" : 90  \
+    , "comments" : "comment comment" \
+    , "rating" : "8.5" \
+    })
+    >>> calisthenic_data.date
+    datetime.date(2020, 6, 21)
+    >>> calisthenic_data.pushups
+    75
+    >>> calisthenic_data.pullups
+    18
+    >>> calisthenic_data.squats
+    90
+    >>> calisthenic_data.comments
+    'comment comment'
+    >>> calisthenic_data.rating
+    '8.5'
+    >>> calisthenic_data.make_data_dict()
+    {'logdate': datetime.date(2020, 6, 21), 'pushups': 75, 'pullups': 18, 'squats': 90, 'comments': 'comment comment', 'rating': '8.5'}
+
+    """
+    def __init__(self, params):
+        self.date     = params.get("logdate")
+        self.pushups  = params.get("pushups")
+        self.pullups  = params.get("pullups")
+        self.squats   = params.get("squats")
+        self.comments = params.get("comments")
+        self.rating   = params.get("rating")
+
+
+    def make_data_dict(self) -> dict:
+        data_dict = {}
+        data_dict["logdate"] = self.date
+        data_dict["pushups"] = self.pushups
+        data_dict["pullups"] = self.pullups
+        data_dict["squats"] = self.squats
+        data_dict["comments"] = self.comments
+        data_dict["rating"] = self.rating
+
+        return data_dict
