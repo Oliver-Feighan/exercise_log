6bb07ffe5577e3fbe4d03df5a6d97514b460a8ab
of15641 <of15641@bristol.ac.uk>
2020-06-24 22:33:20 +0100

    add closing a connection for safety

--- sql_interface/calisthenics.py
+++ sql_interface/calisthenics.py
@@ -13,11 +13,15 @@ class calisthenics(Table):
         """
         :return: no return, just inserts into database
         """
-        cursor = self.connection.cursor()
+        open_connection = self.connection()
+
+        cursor = open_connection.cursor()
 
         sqlite_insert_str = \
             """INSERT INTO %s values (?, ?, ?, ?, ? ,?)""" % self.table_name
 
         cursor.execute(sqlite_insert_str, self.write_data)
 
-        self.connection.commit()
+        open_connection.commit()
+
+        open_connection.close()
--- sql_interface/runs.py
+++ sql_interface/runs.py
@@ -1,8 +1,8 @@
 import sqlite3
-import sql_interface.interface as Table
+from sql_interface.interface import Table
+import datetime
 
-
-class runs(Table):
+class Runs(Table):
     """
     TODO(Oliver) add in specific functions here.
 
@@ -11,7 +11,16 @@ class runs(Table):
     table_name = "runs"
 
     def run_data_check(self):
-        type(self.write_data(0)) ==
+        """
+
+        :return: exits program if
+        """
+        assert(type(self.write_data[0]) == type(datetime.datetime.now().date()))
+        assert(type(self.write_data[1]) == str)
+        assert(type(self.write_data[2]) == int)
+        assert(type(self.write_data[3]) == str)
+        assert(type(self.write_data[4]) == float)
+
 
     def write_to_runs(self):
         """
