ae82e652d91a2d022e054b73366c5e31b3fe58ea
of15641 <of15641@bristol.ac.uk>
2020-07-05 00:13:13 +0100

    try to run outside of pycharm

--- run/main.py
+++ run/main.py
@@ -2,6 +2,7 @@ import gui.master_window
 import sql_interface.runs
 import sql_interface.calisthenics
 
+
 def main():
     run_table_interface = sql_interface.runs.Runs({"file" : "log.db"})
     calisthenics_table_interface = sql_interface.calisthenics.Calisthenics({"file" : "log.db"})
